d462d61a2e2971b11d37ebd5b357849cd8e311a8
Emily <adharaultimasheireen@gmail.com>
2025-07-07 23:54:06 +0200

    .

--- cli.ts
+++ cli.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import prompts from 'prompts';
 
+import fs from 'fs';
 
 async function askAction() {
     const result = await prompts({
         name: 'action',
         type: 'select',
+        message: 'Select an action',
         choices: [{
             title: 'Add component',
             value: 'add_component'
@@ -14,9 +16,28 @@ async function askAction() {
     return result.action;
 }
 
+async function selectComponent() {
+    const result = await prompts({
+        name: 'component',
+        type: 'select',
+        message: 'Select a component',
+        choices: [{
+            title: 'Button',
+            value: 'Button'
+        }]
+    })
+    return result.component;
+}
+
 async function main() {
     const action = await askAction();
-    console.log('ACTION:', action)
+
+
+    if (action === 'add_component') {
+        const component = await selectComponent();
+        fs.copyFileSync(__dirname + `../components/${component}.vue`, `./components/ayaya-ui/${component}.vue`);
+    }
+
 }
 
 main(); 
